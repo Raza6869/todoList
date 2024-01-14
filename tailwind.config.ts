b79318278a55786d43f20805b44a4bf670612b44
Rafael Thumaza <rassraza4@gmail.com>
2024-01-14 07:43:21 -0300

    first config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,33 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        brightBlue: "hsl(220, 98%, 61%)",
+        checkBackground:
+          "linear-gradient hsl(192, 100%, 67%) to hsl(280, 87%, 65%)",
+        veryLightGray: "hsl(0, 0%, 98%)",
+        veryLightGrayishBlue: "hsl(236, 33%, 92%)",
+        lightGrayishBlue: "hsl(233, 11%, 84%)",
+        darkGrayishBlue: "hsl(236, 9%, 61%)",
+
+        veryDarkGrayishBlue: "hsl(235, 19%, 35%)",
+        veryDarkBlue: "hsl(235, 21%, 11%)",
+        veryDarkDesaturatedBlue: "hsl(235, 24%, 19%)",
+        lightGrayishBlueDarkTheme: "hsl(234, 39%, 85%)",
+        lightGrayishBlueHover: "hsl(236, 33%, 92%)",
+        darkGrayishBlueDarkTheme: "hsl(234, 11%, 52%)",
+        veryDarkGrayishBlueDarkTheme1: "hsl(233, 14%, 35%)",
+        veryDarkGrayishBlueDarkTheme2: "hsl(237, 14%, 26%)",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
